e846aee5012b0907a5142facbf3694a829824933
Pavel Senin <senin24@gmail.com>
2018-02-18 02:02:03 +0300

    minor

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,11 +3,15 @@ import map.Island;
 public class App {
 
     public static void main(String[] args) {
-        int wide = 10, height = 10, stones = 10, weapons = 5, takos = 5, zombies = 10, humans = 10, steps = 0;
+        int wide = 10, height = 10,
+                stones = 10, weapons = 5, takos = 5,
+                zombies = 5, humans = 10,
+                steps = 0;
         Island island = new Island(wide, height, stones, weapons, takos, zombies, humans);
         island.printIsland();
         while (island.turn()) {
             steps ++;
+            island.printIsland();
         }
         System.out.println("GAME OVER! Game steps was - " + steps);
     }
